664387e76423c4dc5ada6a2c9ddeccb275b2e2f6
Daniel McAssey <daniel.mcassey@glokon.com>
2015-06-19 20:43:59 +0100

    Initial code

---
+++ Win32/SharedUploader-Watcher/SharedUploader-Watcher/Core/Uploader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace SharedUploader_Watcher.Core
+{
+	class Uploader
+	{
+		const string _CONFIG_FILE = "watcher_config.xml";
+
+		protected void Config_CreateNew()
+		{
+
+		}
+
+		protected void Config_ReadFile()
+		{
+
+		}
+
+		Uploader()
+		{
+
+		}
+	}
+}
---
+++ Win32/SharedUploader-Watcher/SharedUploader-Watcher/Watcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+
+namespace SharedUploader_Watcher
+{
+	public partial class Watcher : ServiceBase
+	{
+		public Watcher()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+		}
+
+		protected override void OnStop()
+		{
+		}
+	}
+}
